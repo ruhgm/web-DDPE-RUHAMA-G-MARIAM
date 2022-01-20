52cdb8f3d24a213e597d93c339c8ad3649651891
ruhgm <88830534+ruhgm@users.noreply.github.com>
2022-01-20 12:19:29 +0300

    Add files via upload

---
+++ form 65675.js
@@ -0,0 +1,67 @@
+function Clear() {
+    window.alert("It is cleared now. Please write again from the beginning");
+}
+
+function Alert() {
+    window.alert("Make sure you have filled the form appropriately.");
+}
+
+function S() {
+    document.write("The form you have filled submitted successfully.")
+}
+
+function myForm() {
+    document.getElementById("form").style.backgroundColor = "green";
+}
+
+function myForm0() {
+    document.getElementById("form").style.backgroundColor = "white";
+}
+
+function myForm1() {
+    document.getElementById("form").style.backgroundColor = "black";
+}
+
+function myForm2() {
+    document.getElementById("form").style.fontSize = "25px";
+}
+
+function myForm3() {
+    document.getElementById("form").style.fontSize = "11px";
+}
+
+function myForm4() {
+    document.getElementById("form").style.fontSize = "16px";
+}
+
+function myForm5() {
+    document.getElementById("form").style.color = "green";
+}
+
+function myForm6() {
+    document.getElementById("form").style.color = "orange";
+}
+
+function myForm7() {
+    document.getElementById("form").style.color = "white";
+}
+
+function myForm8() {
+    document.getElementById("form").style.color = "black";
+}
+
+function myForm9() {
+    document.getElementById("form").style.fontFamily = "monospace";
+}
+
+function myForm10() {
+    document.getElementById("form").style.fontFamily = "Verdana";
+}
+
+function myForm11() {
+    document.getElementById("form").style.fontFamily = "Arial";
+}
+
+function myForm12() {
+    document.getElementById("form").style.fontFamily = "Helvatica";
+}
